543b37d1a0a1c169941ddac6f86f0ab0a6c24a18
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-07-07 17:55:48 +0530

    js statements

--- JS/Basics/one.js
+++ JS/Basics/one.js
@@ -1,2 +1,13 @@
-var x = 10;
-console.log(x);
+var emp_No = 101;
+
+console.log(emp_No);
+
+// single line comments
+/*
+  mutiline comments 
+
+*/
+
+//statement - variable, fun, class, object
+//Javscript statements ends with ';' -optional
+//Js comment are ignored by interpretetor 
